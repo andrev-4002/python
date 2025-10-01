5824301ec14001f0138872b1a92c34d391a7c434
andrev-4002 <andrefelipesantos27@gmail.com>
2025-10-01 16:09:40 -0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,26 @@
+frutas = ["maca", "uva", "pera"]
+#-----------0-------1-------2---
+#--------- -3 ---- -2 ---- -1 --
+
+notasMedia = [7, 8, 10, 5]
+
+print(frutas[2])
+print(frutas[-2])
+
+print(sum(notasMedia))
+print(len(notasMedia), len(frutas))
+
+numeros = [20, 15, 5]
+print(sum(numeros))
+
+y=69
+x = input("pirocão guloso")
+x*y
+if 10*y:
+    piroca="orgia"
+    print(piroca)
+        
+else:
+    piroca="piroca"
+    #o andré é bixa comi sua lixa hahahhahahahah gozei
+    print(piroca)
---
+++ main (2).py
@@ -0,0 +1,14 @@
+frutas = ["maca", "uva", "pera"]
+#-----------0-------1-------2---
+#--------- -3 ---- -2 ---- -1 --
+
+notasMedia = [7, 8, 10, 5]
+
+print(frutas[2])
+print(frutas[-2])
+
+print(sum(notasMedia))
+print(len(notasMedia), len(frutas))
+
+numeros = [20, 15, 5]
+print(sum(numeros))
